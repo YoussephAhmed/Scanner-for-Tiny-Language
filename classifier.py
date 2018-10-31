8fae152903782c81442128b9a0df82bba9b17dee
youssefadelG <33924728+youssefadelG@users.noreply.github.com>
2018-10-31 19:17:39 +0200

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,78 @@
+ #!/usr/bin/python
+from in_out import get_tokens
+import re
+import os
+
+words = ["if" , "then" , "else" , "read" , "end" , "repeat", "write", "until"]
+ 
+tokens = []
+
+typelist = []
+valuelist = []
+
+def isChar(string):
+    match = re.match('[a-zA-Z]',string)
+    if (match):
+        return 1
+    else:
+        return 0
+
+def isNumber(string):
+    match = re.match('[0-9]',string)
+    if (match):
+        return 1
+    else:
+        return 0
+
+def isIdentfier(string):
+    match = re.match('[_a-zA-Z][_a-zA-Z0-9]?',string)
+    if (match):
+        return 1
+    else:
+        return 0
+
+def reservedWords(string):
+    for i in range (0,8) :
+        if (string == words[i]):
+            return 1
+    else:
+        return 0
+
+def isSpecial(string):
+     match = re.match( '[< > * () /- + := - ; / ]' , string)
+     if (match):
+        return 1
+     else:
+        return 0    
+
+def classifier(mylist):
+        for i in range(0,len( mylist)):
+            if (isIdentfier(mylist[i]) and not reservedWords(mylist[i])):
+                typelist.append("Identifier")
+                valuelist.append(mylist[i])
+            elif (reservedWords(mylist[i])):
+                typelist.append("a Reserved word")
+                valuelist.append(mylist[i])
+            elif (isSpecial(mylist[i])):
+                typelist.append("Special character")
+                valuelist.append(mylist[i])
+            elif (isNumber(mylist[i])):
+                typelist.append("a Number")
+                valuelist.append(mylist[i])
+
+
+#def printit(valuetup,typetup):
+def getstr(input):
+    fh = open(input,"r")
+    for line in fh:
+        get_tokens(line)
+    fh.close()
+
+def main():
+    getstr("file.txt")
+    print (tokens)
+    #classifier(tokens)
+    for i in range(0,len(valuelist)):
+        print(valuelist[i] + " is " + typelist[i])
+main()
+
